9643002720075f9e146cc7bf1c043f7226f2cb6a
LittleWhiteLoti <little.white.loti@gmail.com>
2021-10-14 10:37:12 -0400

    Added readFile endpoints

--- server/router.js
+++ server/router.js
@@ -1,12 +1,25 @@
+let fs = require('fs')
+
 const Router = require('express').Router()
 
-Router.post('/users', (req, res) => {
-    console.log(req.body)
+let dataJson
+
+Router.get('/users', (req, res) => {
+    fs.readFile('./db.json', (err, data) => {
+        dataJson = JSON.parse(JSON.stringify(data.toString()))
+        console.log(dataJson)
+        res.setHeader('Content-type', 'application/json')
+        res.send(dataJson).end()
+    })
 })
 
-Router.get('/users/:id', (req, res) => {
-    console.log("Single user endpoint")
-    res.end()
+Router.get('/user/:id', (req, res) => {
+    fs.readFile('./db.json', (err, data) => {
+        dataJson = JSON.parse(JSON.stringify(data.toString()))
+        console.log(dataJson)
+        res.setHeader('Content-type', 'application/json')
+        res.send(dataJson).end()
+    })
 })
 
 Router.get('*', (req, res) => {
